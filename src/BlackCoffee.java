1e3fbd3896d4cfd6167aada59b72141038bfc89b
Mmajko03 <123782744+Mmajko03@users.noreply.github.com>
2023-06-28 17:23:00 -0700

    test

--- src/BlackCoffee.java
+++ src/BlackCoffee.java
@@ -12,7 +12,7 @@ public class BlackCoffee implements Coffee{
         return 1.00;
     }
 
-    public List<String> getIngredients(){
+    public List<String> getIngredients(){ //wiwi
         List<String> blackCoffeeIngredients = new ArrayList<>();
         blackCoffeeIngredients.add("Black Coffee");
 
